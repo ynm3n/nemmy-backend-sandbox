59e8705287b697f2456ec224edcfade4988a3beb
ynm3n <43065779+ynm3n@users.noreply.github.com>
2023-11-21 22:22:47 +0900

    Userモデルを変更

--- internal/user.go
+++ internal/user.go
@@ -9,9 +9,10 @@ import (
 type User struct {
 	bun.BaseModel `bun:"table:users"`
 
-	ID    int64  `json:"userid" bun:"id,pk,nullzero,notnull"`
-	Name  string `json:"username" bun:",nullzero,notnull"`
-	Email string `json:"email" bun:",nullzero,notnull"`
+	ID          int64  `bun:",pk,autoincrement"`
+	Username    string `json:"username" bun:",unique,nullzero,notnull"`
+	DisplayName string `json:"displayName" bun:",nullzero,notnull"`
+	Email       string `json:"email" bun:",unique,nullzero,notnull"`
 
 	CreatedAt time.Time `json:"createdAt" bun:",nullzero,notnull,default:current_timestamp"`
 	UpdatedAt time.Time `json:"updatedAt" bun:",nullzero,notnull,default:current_timestamp"`
