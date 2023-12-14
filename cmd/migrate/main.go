230dd7a79168047afae86ae09e32b6b66f5fd0be
ynm3n <43065779+ynm3n@users.noreply.github.com>
2023-12-14 17:54:35 +0900

    マイグレーション機能の実装
    サーバー起動時以外のタイミングでもマイグレーションしたい場合に使う
    rollbackやstatus、migrateなどのサブコマンドが使える(`cmd/migrate/main.go`内の `newCommands`関数を参照)
    
    `docker compose run --rm migrate サブコマンド`
    のように実行する。Taskfileはまだ書いてない

---
+++ cmd/migrate/main.go
@@ -0,0 +1,136 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/uptrace/bun/extra/bundebug"
+	"github.com/uptrace/bun/migrate"
+	"github.com/ynm3n/go-bun-exercise/internal"
+	"github.com/ynm3n/go-bun-exercise/internal/migrations"
+
+	"github.com/urfave/cli/v2"
+)
+
+func main() {
+	ctx := context.Background()
+	cfg, err := internal.GetConfig()
+	if err != nil {
+		panic(err)
+	}
+	dsn := internal.BuildDSN(cfg)
+	db, err := internal.NewDB(ctx, dsn)
+	if err != nil {
+		panic(err)
+	}
+	db.AddQueryHook(bundebug.NewQueryHook(
+		bundebug.WithEnabled(false),
+		bundebug.FromEnv(""),
+	))
+
+	cmds := newCommands(migrate.NewMigrator(db, migrations.Migrations))
+	app := &cli.App{Commands: cmds}
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func newCommands(migrator *migrate.Migrator) []*cli.Command {
+	return []*cli.Command{
+		{
+			Name:  "init",
+			Usage: "create migration tables",
+			Action: func(c *cli.Context) error {
+				return migrator.Init(c.Context)
+			},
+		},
+		{
+			Name:  "migrate",
+			Usage: "migrate database",
+			Action: func(c *cli.Context) error {
+				if err := migrator.Lock(c.Context); err != nil {
+					return err
+				}
+				defer migrator.Unlock(c.Context) //nolint:errcheck
+
+				group, err := migrator.Migrate(c.Context)
+				if err != nil {
+					return err
+				}
+				if group.IsZero() {
+					fmt.Printf("there are no new migrations to run (database is up to date)\n")
+					return nil
+				}
+				fmt.Printf("migrated to %s\n", group)
+				return nil
+			},
+		},
+		{
+			Name:  "rollback",
+			Usage: "rollback the last migration group",
+			Action: func(c *cli.Context) error {
+				if err := migrator.Lock(c.Context); err != nil {
+					return err
+				}
+				defer migrator.Unlock(c.Context) //nolint:errcheck
+
+				group, err := migrator.Rollback(c.Context)
+				if err != nil {
+					return err
+				}
+				if group.IsZero() {
+					fmt.Printf("there are no groups to roll back\n")
+					return nil
+				}
+				fmt.Printf("rolled back %s\n", group)
+				return nil
+			},
+		},
+		{
+			Name:  "lock",
+			Usage: "lock migrations",
+			Action: func(c *cli.Context) error {
+				return migrator.Lock(c.Context)
+			},
+		},
+		{
+			Name:  "unlock",
+			Usage: "unlock migrations",
+			Action: func(c *cli.Context) error {
+				return migrator.Unlock(c.Context)
+			},
+		},
+		{
+			Name:  "status",
+			Usage: "print migrations status",
+			Action: func(c *cli.Context) error {
+				ms, err := migrator.MigrationsWithStatus(c.Context)
+				if err != nil {
+					return err
+				}
+				fmt.Printf("migrations: %s\n", ms)
+				fmt.Printf("unapplied migrations: %s\n", ms.Unapplied())
+				fmt.Printf("last migration group: %s\n", ms.LastGroup())
+				return nil
+			},
+		},
+		{
+			Name:  "mark_applied",
+			Usage: "mark migrations as applied without actually running them",
+			Action: func(c *cli.Context) error {
+				group, err := migrator.Migrate(c.Context, migrate.WithNopMigration())
+				if err != nil {
+					return err
+				}
+				if group.IsZero() {
+					fmt.Printf("there are no new migrations to mark as applied\n")
+					return nil
+				}
+				fmt.Printf("marked as applied %s\n", group)
+				return nil
+			},
+		},
+	}
+}
