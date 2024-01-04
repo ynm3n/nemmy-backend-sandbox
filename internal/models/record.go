f78a3b19813ebfab4c0a7bdb346522b30d55dfcb
ynm3n <43065779+ynm3n@users.noreply.github.com>
2024-01-04 16:01:35 +0900

    RecordモデルのStartedAtフィールドのデフォルト値としてタイムスタンプを使う

--- internal/models/record.go
+++ internal/models/record.go
@@ -11,7 +11,7 @@ type Record struct {
 
 	ID        int64     `json:"-" bun:",pk,autoincrement"`
 	SubjectID int64     `json:"-" bun:",nullzero,notnull"`
-	StartedAt time.Time `json:"startedAt" bun:",nullzero,notnull"`
+	StartedAt time.Time `json:"startedAt" bun:",nullzero,notnull,default:current_timestamp"`
 	Duration  int64     `json:"duration" bun:",notnull"`
 	Comment   string    `json:"comment" bun:",type:text"`
 
